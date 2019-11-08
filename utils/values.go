286cf8654fe7c51c3353da1fc07dbe7a38b2c95e
nuelsoft <nuel.mailbox@gmail.com>
2019-11-08 21:33:55 +0100

    first commit

--- utils/values.go
+++ utils/values.go
@@ -1,11 +1,11 @@
 package utils
 
 const (
-	ConsumerKey       = "dWdV9gIgvgsZIY3oQdrMAPsMV"
-	ConsumerSecret    = "ANCAOWOnaElrpq7u1omTVnJlbfl7zfB2H2cqLRSYoPwKLEuWrW"
-	AccessToken       = "902819686630424576-OQFolLdhjXOPQBcOAIhewPdBCaDv5Nh"
-	AccessTokenSecret = "bri5fIjZMab3Yk6rGR3fGmmXy5IzCm2Npql6ZL9dMQi47"
-	BenchID           = "1192891762932629511"
-	Count             = "1000000000"
-	Query             = "DevFestAba"
+	ConsumerKey       = "YOUR_CONSUMER_KEY"
+	ConsumerSecret    = "YOUR_CONSUMER_SECRET"
+	AccessToken       = "YOUR_ACCESS_TOKEN"
+	AccessTokenSecret = "YOUR_ACCESS_TOKEN_SECRET"
+	BenchID           = "BENCH_ID"
+	Count             = "COUNT"
+	Query             = "QUERY"
 )
